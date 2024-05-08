2ba453035c5b821fcc258399abc65951e8f8ff7c
Ryan Augustinsky <ryan@augustinsky.ca>
2024-05-08 04:35:05 -0400

    added get_actions() to actions endpoint

--- api/action_endpoint.py
+++ api/action_endpoint.py
@@ -2,6 +2,8 @@
 action_endpoint.py
 """
 from api.endpoint import Endpoint
+from action.action import Action
+from action.action_parse import parse_action
 
 class ActionEndpoint(Endpoint):
     """
@@ -10,3 +12,30 @@ class ActionEndpoint(Endpoint):
 
     def __init__(self):
         super().__init__('action')
+
+    def get_actions(self) -> list[Action]:
+        """
+        Get all actions.
+
+        Returns:
+            dict: The response from the server.
+        """
+        json = self.get()
+
+        actions = []
+        for entry in json:
+            # validate that the entry is a dictionary and has a klass and payload key
+            if not isinstance(entry, dict):
+                raise ValueError('Entry must be a dictionary')
+
+            if 'klass' not in entry:
+                raise ValueError('Entry must have a klass key')
+
+            if 'payload' not in entry:
+                raise ValueError('Entry must have a payload key')
+
+            klass = parse_action(entry['klass'])
+            payload = entry['payload']
+            actions.append(klass(payload))
+
+        return actions
